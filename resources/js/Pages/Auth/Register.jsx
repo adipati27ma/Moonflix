c2f166dcc1173bafc5c3f425d39ea2f48ad7e30d
adipati27ma <adipati.cs.samsung@gmail.com>
2024-02-05 15:50:35 +0700

    feat: integration sign up

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,125 +1,133 @@
 import { useEffect } from "react";
-import GuestLayout from "@/Layouts/GuestLayout";
-import InputError from "@/Components/InputError";
+import Button from "@/Components/Button";
 import InputLabel from "@/Components/InputLabel";
-import PrimaryButton from "@/Components/Button";
 import TextInput from "@/Components/TextInput";
+import InputError from "@/Components/InputError";
 import { Head, Link, useForm } from "@inertiajs/react";
+import React from "react";
 
-export default function Register() {
-    const { data, setData, post, processing, errors, reset } = useForm({
-        name: "",
-        email: "",
-        password: "",
-        password_confirmation: "",
-    });
+const Register = () => {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+  });
 
-    useEffect(() => {
-        return () => {
-            reset("password", "password_confirmation");
-        };
-    }, []);
-
-    const submit = (e) => {
-        e.preventDefault();
-
-        post(route("register"));
+  useEffect(() => {
+    return () => {
+      reset("password", "password_confirmation");
     };
+  }, []);
 
-    return (
-        <GuestLayout>
-            <Head title="Register" />
+  const submit = (e) => {
+    e.preventDefault();
 
-            <form onSubmit={submit}>
+    post(route("register"));
+  };
+
+  return (
+    <>
+      <Head title="Sign Up" />
+      <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+        <div className="fixed top-[-50px] hidden lg:block">
+          <img
+            src="/images/signup-image.png"
+            className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]"
+            alt=""
+          />
+        </div>
+        <div className="py-24 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+          <div>
+            <img src="/images/moonton-white.svg" alt="" />
+            <div className="my-[70px]">
+              <div className="font-semibold text-[26px] mb-3">Sign Up</div>
+              <p className="text-base text-[#767676] leading-7">
+                Explore our new movies and get <br />
+                the better insight for your life
+              </p>
+            </div>
+            <form className="w-[370px]" onSubmit={submit}>
+              <div className="flex flex-col gap-6">
                 <div>
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
-                        id="name"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        onChange={(e) => setData("name", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
+                  <InputLabel forInput="name" value="Full Name" />
+                  <TextInput
+                    type="text"
+                    name="name"
+                    value={data.name}
+                    placeholder="Your name..."
+                    isFocused={true}
+                    onChange={(e) => setData("name", e.target.value)}
+                    required
+                  />
+                  <InputError message={errors.name} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData("email", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
+                <div>
+                  <InputLabel forInput="email" value="Email Address" />
+                  <TextInput
+                    type="email"
+                    name="email"
+                    value={data.email}
+                    placeholder="Your Email Address"
+                    isFocused={true}
+                    onChange={(e) => setData("email", e.target.value)}
+                    required
+                  />
+                  <InputError message={errors.email} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
-
-                    <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData("password", e.target.value)}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
+                <div>
+                  <InputLabel forInput="password" value="Password" />
+                  <TextInput
+                    type="password"
+                    name="password"
+                    value={data.password}
+                    placeholder="Your Password"
+                    isFocused={true}
+                    onChange={(e) => setData("password", e.target.value)}
+                    required
+                  />
+                  <InputError message={errors.password} className="mt-2" />
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel
-                        htmlFor="password_confirmation"
-                        value="Confirm Password"
-                    />
-
-                    <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) =>
-                            setData("password_confirmation", e.target.value)
-                        }
-                        required
-                    />
-
-                    <InputError
-                        message={errors.password_confirmation}
-                        className="mt-2"
-                    />
+                <div>
+                  <InputLabel
+                    forInput="password confirmation"
+                    value="Confirm Password"
+                  />
+                  <TextInput
+                    type="password"
+                    name="password confirmation"
+                    value={data.password_confirmation}
+                    isFocused={true}
+                    onChange={(e) =>
+                      setData("password_confirmation", e.target.value)
+                    }
+                    required
+                  />
+                  <InputError message={errors.password} className="mt-2" />
                 </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link
-                        href={route("login")}
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ms-4" disabled={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
+              </div>
+              <div className="grid space-y-[14px] mt-[30px]">
+                <Button isLoading={processing} type="submit" variant="primary">
+                  <span className="text-base font-semibold">Sign Up</span>
+                </Button>
+                <Link href={route("login")}>
+                  <Button
+                    type="button"
+                    variant="light-outline"
+                    isLoading={processing}
+                  >
+                    <span className="text-base font-semibold">
+                      Sign In to My Account
+                    </span>
+                  </Button>
+                </Link>
+              </div>
             </form>
-        </GuestLayout>
-    );
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,30 @@ import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.jsx",
-    ],
+  content: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.jsx",
+  ],
 
-    theme: {
-        fontFamily: {
-            poppins: "Poppins, sans-serif",
-        },
-        extend: {
-            colors: {
-                alerange: "#FB6908",
-                "gray-1": "#B4B4B4",
-                "gray-2": "#E2E0E0",
-                "form-bg": "#212121",
-            },
-            screens: {
-                laptopLg: "1160px",
-                laptopXl: "1360px",
-            },
-        },
+  theme: {
+    fontFamily: {
+      poppins: "Poppins, sans-serif",
     },
+    extend: {
+      colors: {
+        alerange: "#FB6908",
+        "gray-1": "#B4B4B4",
+        "gray-2": "#E2E0E0",
+        "form-bg": "#212121",
+      },
+      screens: {
+        laptopLg: "1160px",
+        laptopXl: "1360px",
+      },
+    },
+  },
 
-    plugins: [forms],
+  plugins: [forms],
 };
